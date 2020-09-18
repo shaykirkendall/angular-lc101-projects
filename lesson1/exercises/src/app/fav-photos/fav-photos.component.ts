80ae6b1894a4c2590cd807ccd1d56a4a00b3a2ad
Shay <shay.kirkendall@gmail.com>
2020-09-17 19:36:47 -0500

    Exercise 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'This is the Good Stuff';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://st3.depositphotos.com/thumbs/13194036/image/18562/185623840/api_thumb_450.jpg'
+  image3 = 'https://st.depositphotos.com/thumbs/1009092/image/4149/41494311/api_thumb_450.jpg';
 
   constructor() { }
 
